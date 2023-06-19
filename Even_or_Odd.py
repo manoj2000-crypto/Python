fe45c543b06cab5bedf305777542b08c762b0487
Manoj Kale <manojkale2000@gmail.com>
2023-06-19 08:50:31 +0530

    Added formated string to get variable values inside string

--- Even_or_Odd.py
+++ Even_or_Odd.py
@@ -4,10 +4,10 @@
 num = int(input('Enter a number : '))
 
 if (num % 2) == 0:
-           print ('The {num} is even.')
+           print (f'The {num} is even.')
 
 else:
-           print ('The provided {num} is odd.')
+           print (f'The provided {num} is odd.')
 
 
 
