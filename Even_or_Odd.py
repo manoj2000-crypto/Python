1a763e6c8ed7b5d3104853b7fbd8633141f50a5c
Manoj Kale <manojkale2000@gmail.com>
2023-06-19 08:46:41 +0530

    Even or Odd Program

---
+++ Even_or_Odd.py
@@ -0,0 +1,41 @@
+#Write a program to check the number entered by user is even or odd. 
+#Program should accept integer digits only.
+
+num = int(input('Enter a number : '))
+
+if (num % 2) == 0:
+           print ('The {num} is even.')
+
+else:
+           print ('The provided {num} is odd.')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
